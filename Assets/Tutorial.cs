9ef4dae98789dd4db0af1a0307dfbf7ffa4ad47d
matt <matthew_fyfe@hotmail.com>
2017-05-20 22:11:06 -0600

    updates to tutorials

--- Assets/Tutorial.cs
+++ Assets/Tutorial.cs
@@ -5,18 +5,35 @@ using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour {
 
+	private Text texty;
+
+
 	// Use this for initialization
 	void Start () {
-		
+		texty = GetComponent<Text>();
+		Tutorial0 ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Invoke ("Tutorial1", 3);
+
+	}
+
+	void blank()
+	{
+		texty.text = "";
+	}
+
+	void Tutorial0()
+	{
+		texty.text = "Use WASD to move around!";
+		Invoke ("blank", 5);
+		Invoke ("Tutorial1", 7);
 	}
 
 	void Tutorial1()
 	{
-		GetComponent<Text> ().text = "asdd";
+		texty.text = "Hit Spacebar to re-center camera.";
+		Invoke ("blank", 5);
 	}
 }
