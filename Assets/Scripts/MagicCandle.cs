7067f49c6a7641c8867ac2059441c3c0c7cbe5a1
matt <matthew_fyfe@hotmail.com>
2017-05-20 19:53:13 -0600

    Added magic candle and hammer

---
+++ Assets/Scripts/MagicCandle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicCandle : MonoBehaviour {
+
+	public GameObject MagicLight;
+	private Vector3 newPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp ("space")) {
+			//drop light
+			newPosition = transform.position;
+			newPosition.y += 2;
+			MagicLight.transform.position = newPosition;
+		}
+	}
+}
---
+++ Assets/Scripts/TheHammer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheHammer : MonoBehaviour {
+
+	public GameObject hammer;
+	private Vector3 newPosition;
+	private Rigidbody hammerBody;
+
+	// Use this for initialization
+	void Start () {
+		hammerBody = hammer.GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp ("space")) {
+			//respawn hammer
+			newPosition = transform.position;
+			newPosition.y += -0.5f;
+			hammer.transform.position = newPosition;
+
+			//throw hammer
+			hammerBody.rotation = Quaternion.identity;
+			hammerBody.velocity = Vector3.zero;
+			hammerBody.AddForce(transform.forward * 25, ForceMode.Impulse);
+		}
+	}
+}
